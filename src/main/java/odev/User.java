d1516ce431dab554f68e4451700075809cf9b2af
sezer1578 <sezer_ozaltin@hotmail.com>
2017-05-02 21:26:28 +0300

    YazilimGelistirmeAraclari
    SezerOzaltun

---
+++ src/main/java/odev/User.java
@@ -0,0 +1,17 @@
+package odev;
+
+/**
+ * Created by SEZER on 2.05.2017.
+ */
+public class User {
+    private String userName;
+    private int password;
+
+    public  User(String userName, int password)
+    {
+        this.userName = userName;
+        this.password = password;
+
+
+    }
+}
---
+++ src/test/java/UserTest.java
@@ -0,0 +1,18 @@
+import odev.User;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by SEZER on 2.05.2017.
+ */
+public class UserTest {
+
+    @Test
+    public void userTest()
+    {
+        final User user = new User("sezer", 123);
+        assertTrue(true);
+    }
+
+}
